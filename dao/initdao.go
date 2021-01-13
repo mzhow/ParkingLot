d18c673607fc8ffc205a4424f05c8957eed5b438
mzhow <mzhow@outlook.com>
2021-01-13 11:23:09 +0800

    Update SQL module

--- dao/initdao.go
+++ dao/initdao.go
@@ -15,14 +15,14 @@ func init() {
 
 	// 普通用户数据库
 	DB, err = sql.Open("mysql",
-		"ParkingLotUser:zeVym.04wcTni.03@tcp(47.97.82.144:3306)/ParkingLot?charset=utf8&parseTime=true&loc=Local")
+		"Username:Password@tcp(47.97.82.144:3306)/ParkingLot?charset=utf8&parseTime=true&loc=Local")
 	if err != nil {
 		fmt.Println(err)
 	}
 
 	// 管理员数据库
 	DBAdmin, err = sql.Open("mysql",
-		"ParkingLotAdmin:Qzect.03yibm@tcp(47.97.82.144:3306)/ParkingLot?charset=utf8&parseTime=true&loc=Local")
+		"Username:Password@tcp(47.97.82.144:3306)/ParkingLot?charset=utf8&parseTime=true&loc=Local")
 	if err != nil {
 		fmt.Println(err)
 	}
